a9766d0145657273bb3e62317b3c88398024b938
m2za5 <2renyeagero33o@gmail.com>
2026-03-26 23:44:53 +0900

    feat: Algorithms 1003

---
+++ Baekjoon/Al1003.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+int fibonacci(int n);
+
+int main()
+{
+    int n;
+    if (!(cin >> n)) return 0;
+
+    cout << "결과: " << fibonacci(n) << endl;
+
+    return 0;
+}
+
+int fibonacci(int n) {
+    if (n == 0) {
+        printf("0");
+        return 0;
+    }
+    else if (n == 1) {
+        printf("1");
+        return 1;
+    }
+    else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
